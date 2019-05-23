59b3da6b0a0ceac648cfb88767c8778b27b682e7
Josemanie <50826375+Josemanie@users.noreply.github.com>
2019-05-23 08:26:37 +0700

    Create config.php

---
+++ bermi/config.php
@@ -0,0 +1,6 @@
+<?php
+$user_hash = "e54d98eb-5d5e-4ec3-ae2e-24d7260633d9";
+$accept_language = "en-us";
+$user_agent = "Dalvik/2.1.0 (Linux; U; Android 5.1.1; hm note 1lte Build/LMY47I)";
+$cookie = "user_hash=2|1:0|10:1556157596|9:user_hash|48:ZTU0ZDk4ZWItNWQ1ZS00ZWMzLWFlMmUtMjRkNzI2MDYzM2Q5|f5e1d99c31d188917908a4ff9b703448fc73c3a4528add854bea8ee10e398fec";
+?>
