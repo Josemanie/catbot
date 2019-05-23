79cd5907303da5e11787985cb9bc3a7d6587f0e7
Josemanie <50826375+Josemanie@users.noreply.github.com>
2019-05-23 08:35:20 +0700

    Create config.php

---
+++ Qriket/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$email = "letteralarm@gmail.com";
+$password = "Wasd1111";
+
+//Pillih Warna 0 untuk hijau 1 untuk kuning
+$collor = "0";
+
+// Pilih Jumlah Spin Saran 1 - 5
+$amount = "1";
+
+
+
