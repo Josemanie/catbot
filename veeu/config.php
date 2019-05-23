fce915ccd18aa3c4d5af22b67964e97bf1605fce
Josemanie <50826375+Josemanie@users.noreply.github.com>
2019-05-23 08:38:49 +0700

    Create config.php

---
+++ veeu/config.php
@@ -0,0 +1,12 @@
+<?php
+//delay min 1 sec max 9999999 sec :v
+$delay=15;
+//$config = ["token",];
+$config = [
+	   "d5ae2352-9aad-422d-a2b4-8fd557d2a21c",
+	   "44a618e3-37f6-4a98-a4a7-06ea48e3bf2a",
+
+
+];
+
+
